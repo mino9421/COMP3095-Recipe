54d2190e119f9d96c85ee218602b174093ea5c05
mino9421 <mino9421@gmail.com>
2021-10-29 18:51:11 -0400

    POJO - recipe

--- src/test/java/ca/gbc/comp3095/food_recipe/FoodRecipeApplicationTests.java
+++ src/test/java/ca/gbc/comp3095/food_recipe/FoodRecipeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FoodRecipeApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
