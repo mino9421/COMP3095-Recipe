49430abe0d44b0d38c907f505571a8ae869f0467
Melody Nguyen <78733227+Melody1402@users.noreply.github.com>
2021-10-30 15:12:31 -0400

    Add more files for UI

--- src/main/java/ca/gbc/comp3095/food_recipe/controllers/RecipeController.java
+++ src/main/java/ca/gbc/comp3095/food_recipe/controllers/RecipeController.java
@@ -1,6 +1,7 @@
 package ca.gbc.comp3095.food_recipe.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import ca.gbc.comp3095.food_recipe.model.Recipe;
 import ca.gbc.comp3095.food_recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,15 +21,20 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
         System.out.println("Inside Recipe Controller....with id value="+id);
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
-        return "recipe/view";
+        return "view";
 
     }
+    @GetMapping("recipe/add")
+    public String addRecipe(Model model){
+        model.addAttribute("recipe", new Recipe());
 
+        return "addRecipe";
+    }
 
     @GetMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
 
-        return "recipe/update";
+        return "update";
     }
 
     @GetMapping ("recipe/{id}/delete")
--- src/main/java/ca/gbc/comp3095/food_recipe/model/Recipe.java
+++ src/main/java/ca/gbc/comp3095/food_recipe/model/Recipe.java
@@ -1,87 +1,23 @@
 package ca.gbc.comp3095.food_recipe.model;
 
-import javax.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class Recipe {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String description;
     private Integer prepTime;
     private Integer cookTime;
     private Integer serving;
-    @Lob
     private String instructions;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    @Lob
+    private Set<Ingredient> ingredients = new HashSet<>();
     private Byte[] image;
-
-    @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getPrepTime() {
-        return prepTime;
-    }
-
-    public void setPrepTime(Integer prepTime) {
-        this.prepTime = prepTime;
-    }
-
-    public Integer getCookTime() {
-        return cookTime;
-    }
-
-    public void setCookTime(Integer cookTime) {
-        this.cookTime = cookTime;
-    }
-
-    public Integer getServing() {
-        return serving;
-    }
-
-    public void setServing(Integer serving) {
-        this.serving = serving;
-    }
-
-    public String getInstructions() {
-        return instructions;
-    }
-
-    public void setInstructions(String instructions) {
-        this.instructions = instructions;
-    }
-
-    public Byte[] getImage() {
-        return image;
-    }
-
-    public void setImage(Byte[] image) {
-        this.image = image;
-    }
-
-    public Difficulty getDifficulty() {
-        return difficulty;
-    }
-
-    public void setDifficulty(Difficulty difficulty) {
-        this.difficulty = difficulty;
-    }
 }
