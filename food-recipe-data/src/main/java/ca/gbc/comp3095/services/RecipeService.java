690b1e61f5cc6ada0b1f4a2eeb63d64111e0feb3
mino9421 <mino9421@gmail.com>
2021-10-31 15:46:02 -0400

    modified RecipeService
    debugged an error by adding 2 annotations

--- food-recipe-data/src/main/java/ca/gbc/comp3095/services/RecipeService.java
+++ food-recipe-data/src/main/java/ca/gbc/comp3095/services/RecipeService.java
@@ -1,8 +1,13 @@
 package ca.gbc.comp3095.services;
 
 import ca.gbc.comp3095.model.Recipe;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import javax.persistence.Entity;
 import java.util.Set;
+
+@Entity(name = "ca.gbc.comp3095.data.jpa.entity")       // Armen added these 2 annotations as a solution not 100% sure
+@EnableJpaRepositories("ca.gbc.comp3095.data.jpa.dao")
 
 public interface RecipeService {
     Set<Recipe> getRecipes();
