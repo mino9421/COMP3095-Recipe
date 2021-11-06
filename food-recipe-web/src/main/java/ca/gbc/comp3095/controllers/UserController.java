89b9defcde213e6e3208c86a0de1b346ea2088bc
mino9421 <mino9421@gmail.com>
2021-11-06 15:12:26 -0400

    presenting

--- food-recipe-web/src/main/java/ca/gbc/comp3095/controllers/UserController.java
+++ food-recipe-web/src/main/java/ca/gbc/comp3095/controllers/UserController.java
@@ -1,6 +1,8 @@
 package ca.gbc.comp3095.controllers;
 
+import ca.gbc.comp3095.services.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/users")
@@ -9,8 +11,15 @@ public class UserController {
 
     // localhost:8080/dashboard.html
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @RequestMapping({"", "/", "/dashboard", "/dashboard.html"})
-    public String listUser(){
+    public String listUser(Model model){
+        model.addAttribute("users", userService.findAll());
         return "users/dashboard";
     }
 }
