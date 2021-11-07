f83fdc5d1e637d2663f86db3b5c9d2bae577e2e7
mino9421 <mino9421@gmail.com>
2021-11-07 12:49:52 -0500

    modifying files

--- food-recipe-data/src/main/java/ca/gbc/comp3095/repositories/RecipeRepository.java
+++ food-recipe-data/src/main/java/ca/gbc/comp3095/repositories/RecipeRepository.java
@@ -1,10 +1,10 @@
 package ca.gbc.comp3095.repositories;
 
 import ca.gbc.comp3095.model.Recipe;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-//@EnableJpaRepositories
+@Repository
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 
 }
