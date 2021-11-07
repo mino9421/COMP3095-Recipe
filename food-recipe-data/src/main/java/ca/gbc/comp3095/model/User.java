00cccc265661029669f79a2ec26d2cf9dd7a0357
Melody Nguyen <78733227+Melody1402@users.noreply.github.com>
2021-11-07 16:14:58 -0500

    table user

--- food-recipe-data/src/main/java/ca/gbc/comp3095/model/User.java
+++ food-recipe-data/src/main/java/ca/gbc/comp3095/model/User.java
@@ -1,14 +1,46 @@
 package ca.gbc.comp3095.model;
 
-public class User extends Person{
+import lombok.Data;
 
+import javax.persistence.*;
+
+
+@Data
+@Entity
+@Table(name = "user")
+public class User extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    public Long getId(){return id;}
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private String firstname;
+    private String lastname;
     private String username;
     private String password;
     private String email;
 
-    public String getEmail() { return email; }
+    public String getFirstname() {
+        return firstname;
+    }
 
-    public void setEmail(String email) { this.email = email; }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 
     public String getUsername() {
         return username;
@@ -24,5 +56,13 @@ public class User extends Person{
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
