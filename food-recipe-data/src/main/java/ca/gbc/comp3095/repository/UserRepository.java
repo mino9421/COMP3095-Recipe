d1775b5c7c0d4a4e5d0f987cb19f89e9fdccfa94
mino9421 <mino9421@gmail.com>
2021-11-07 22:33:20 -0500

    Create UserRepository.java

---
+++ food-recipe-data/src/main/java/ca/gbc/comp3095/repository/UserRepository.java
@@ -0,0 +1,30 @@
+/*
+ * Project: food recipe
+ * Assignment:  assignment 1
+ * Author(s):   Armen Levon Armen
+ * Student Number: 101281931
+ * Date: 11/7/2021 - mm/dd/yy
+ * Description: This file supposedly makes connection with the database and sends queries with appropriate methods
+ *                  but unfortunately there are too many student on my wagon and its too heavy to push
+ */
+
+//package ca.gbc.comp3095.repository;
+//
+//import ca.gbc.comp3095.model.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//
+//@Repository
+//public interface UserRepository extends JpaRepository<User, Long> {
+//
+////    @Query("SELECT '*' FROM 'USER'")
+////    public List<User> searchAll();
+//}
+//
+//
+////    INSERT INTO USER (ID , EMAIL , FIRSTNAME , LASTNAME , PASSWORD , USERNAME )
+////    VALUES ('1', 'mino@gg.com', 'mino', 'levon', 'mino123', 'minolevon');
